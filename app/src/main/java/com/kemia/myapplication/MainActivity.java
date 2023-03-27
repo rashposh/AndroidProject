ccf9bf66940534c1e84155d516864a39c6a29ccd
PTA123 <anphanvip09@gmail.com>
2023-03-27 12:12:02 +0700

    change name app

--- app/src/main/java/com/kemia/myapplication/MainActivity.java
+++ app/src/main/java/com/kemia/myapplication/MainActivity.java
@@ -1,53 +1,154 @@
+//package com.kemia.myapplication;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.view.Window;
+//import android.widget.Button;
+//import android.widget.SearchView;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    Button more;
+////    SearchView search;
+//    Button bao1;
+//    Button trend;
+//    Button like;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getSupportActionBar().hide();
+//        setContentView(R.layout.activity_main);
+//        more = findViewById(R.id.more);
+////        search = findViewById(R.id.search);
+//
+//        bao1.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, BaoMoiActivity.class));
+//            }
+//        }
+//        );
+//        /**video.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, VideoActivity.class));
+//            }
+//        }
+//        )*/
+//        trend.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {setContentView(R.layout.baomoi);}
+//        }
+//        );
+//        like.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {setContentView(R.layout.baomoi);}
+//        }
+//        );
+//    }
+//}
+
+
+
+
 package com.kemia.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.SearchView;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.kemia.myapplication.fragment.TinTucFragment;
+import com.kemia.myapplication.fragment.CaNhanFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button more;
-    SearchView search;
-    Button bao1;
-    Button trend;
-    Button video;
-    Button like;
-
+    BottomNavigationView mnBottom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature((Window.FEATURE_NO_TITLE));
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        more = findViewById(R.id.more);
-        search = findViewById(R.id.search);
+        Fragment fmnew = new TinTucFragment();
+        loadFragment(fmnew);
+        mnBottom = findViewById(R.id.bottomNavigationView);
+        //
 
-        bao1.setOnClickListener(new View.OnClickListener(){
+        // Load lên Fragment
+        mnBottom.setOnItemSelectedListener(getListener());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+        return true;
+    }
+
+    @NonNull
+    private NavigationBarView.OnItemSelectedListener getListener()
+    {
+        return new NavigationBarView.OnItemSelectedListener()
+        {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, BaoMoiActivity.class));
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                Fragment fmNew;
+                switch (item.getItemId())
+                {
+                    case R.id.self:
+                        getSupportActionBar().setTitle(item.getTitle());
+                        fmNew = new TinTucFragment();
+                        loadFragment(fmNew);
+                        return true;
+
+                    case R.id.cc:
+                        getSupportActionBar().setTitle(item.getTitle());
+                        fmNew = new CaNhanFragment();
+                        loadFragment(fmNew);
+                        return true;
+                }
+                return true;
             }
-        }
-        );
-        /**video.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, VideoActivity.class));
-            }
-        }
-        )*/
-        trend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {setContentView(R.layout.baomoi);}
-        }
-        );
-        like.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {setContentView(R.layout.baomoi);}
-        }
-        );
+        };
+    }
+
+    // Hàm load fragment
+    void loadFragment(Fragment fmNew) {
+        FragmentTransaction fmTran = getSupportFragmentManager().beginTransaction();
+        fmTran.replace(R.id.main_fragment, fmNew);
+        fmTran.addToBackStack(null);
+        fmTran.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
+
+
+
+
+
+
+
--- app/src/main/java/com/kemia/myapplication/fragment/TinTucFragment.java
+++ app/src/main/java/com/kemia/myapplication/fragment/TinTucFragment.java
@@ -1,4 +1,4 @@
-package com.kemia.myapplication;
+package com.kemia.myapplication.fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.kemia.myapplication.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link canhan#newInstance} factory method to
+ * Use the {@link TinTucFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class canhan extends Fragment {
+public class TinTucFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class canhan extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public canhan() {
+    public TinTucFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class canhan extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment canhan.
+     * @return A new instance of fragment TinTucFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static canhan newInstance(String param1, String param2) {
-        canhan fragment = new canhan();
+    public static TinTucFragment newInstance(String param1, String param2) {
+        TinTucFragment fragment = new TinTucFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class canhan extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_canhan, container, false);
+        return inflater.inflate(R.layout.fragment_tin_tuc, container, false);
     }
 }
