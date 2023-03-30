1bd7c1fb2ec97ab5de951a6853c4cabee752e234
PTA123 <anphanvip09@gmail.com>
2023-03-30 08:46:24 +0700

    change name app

--- app/src/main/java/com/kemia/myapplication/fragment_tuychon/moi.java
+++ app/src/main/java/com/kemia/myapplication/fragment_tuychon/moi.java
@@ -1,4 +1,4 @@
-package com.kemia.myapplication;
+package com.kemia.myapplication.fragment_tuychon;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.kemia.myapplication.R;
 
 /**
  * A simple {@link Fragment} subclass.
